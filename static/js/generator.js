2fcc6a3d0934c9d02a9fc4613da6389ddcb9a86d
lilvedaes <daniela.venturo@utec.edu.pe>
2019-09-19 21:23:57 -0400

    function works

--- static/js/generator.js
+++ static/js/generator.js
@@ -2,5 +2,5 @@ function generates(){
     document.getElementById("fillName").innerHTML = document.querySelector('#fillForm [name="name"]').value;
     document.getElementById("fillPosition").innerHTML = document.querySelector('#fillForm [name="position"]').value;
     document.getElementById("fillPhone").innerHTML = document.querySelector('#fillForm [name="phone"]').value;
-    document.getElementById("fillLink").href.innerHTML = document.querySelector('#fillForm [name="linkedinURL"]').value;
+    document.getElementById("fillLink").setAttribute("href", document.querySelector('#fillForm [name="linkedinURL"]').value);
 }
